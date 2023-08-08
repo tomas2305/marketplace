005fa513ab09ccdcf5a12c5d7c99cfbf63287e5e
tomas2305 <tomas23058@gmail.com>
2023-08-07 23:05:25 -0300

    apis && axios

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
 import { Button, Container } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { HeaderComponent } from "../../components";
+import { characters } from "../../api/characters";
 
 export const HomePage: React.FC<{}> = () => {
 
+  useEffect(()=>{
+    characters.getById({id:1}).then(r => {
+      console.log(r.data);
+    }).catch(err => {console.log(err);});
+  },[])
 
   return (
     <Container maxWidth="xl">
